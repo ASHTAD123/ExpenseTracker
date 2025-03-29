446df3c9daf0cbaa718e6fcdc2493777683a1de6
ASHTAD IRANI <ashtadcyrusirani@gmail.com>
2025-03-30 01:26:58 +0530

    changes

--- ExpenseTracker_FrontEnd/vite.config.js
+++ ExpenseTracker_FrontEnd/vite.config.js
@@ -2,16 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-
-  server: {
-    proxy: {
-        "/api": {
-            target: import.meta.env.VITE_API_URL,
-            changeOrigin: true,
-            secure: true,
-            credentials: true,
-        },
-    },
-},
     plugins: [react()]
 })
