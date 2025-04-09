8f03ba4336202358d4dd4707d16e693368b9fe76
ASHTAD IRANI <ashtadcyrusirani@gmail.com>
2025-04-09 18:03:26 +0530

    trying to fix

--- src/main/java/com/example/expense/Demo1Application.java
+++ src/main/java/com/example/expense/Demo1Application.java
@@ -1,10 +1,11 @@
 package com.example.expense;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.expense.Entities")
 public class Demo1Application {
 	
 
