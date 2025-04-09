24e2294dc359ea822ed9cd110f1901a7d6280d43
ASHTAD IRANI <ashtadcyrusirani@gmail.com>
2025-04-10 00:18:14 +0530

    cors-config-2

--- src/main/java/com/example/expense/WebConfig.java
+++ src/main/java/com/example/expense/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        		.allowedOrigins("https://expensetracker-gxy7.onrender.com") 
+        		.allowedOrigins("symphonious-otter-07cb0e.netlify.app") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .allowedHeaders("Authorization", "Content-Type")
