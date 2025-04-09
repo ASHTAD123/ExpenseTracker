b99d70f29097428831344e10ba0014ef7183550a
ASHTAD IRANI <ashtadcyrusirani@gmail.com>
2025-04-09 11:48:46 +0530

    changed_app.prod file

--- src/main/java/com/example/expense/Demo1Application.java
+++ src/main/java/com/example/expense/Demo1Application.java
@@ -1,5 +1,4 @@
-	package com.example.expense;
-
+package com.example.expense;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
